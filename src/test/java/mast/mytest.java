cc2b59b106bae7ac11f98bd2128dd242c57d183f
mast <304737254@qq.com>
2020-07-05 11:06:20 +0800

    10:46 commit  mast-ncc

---
+++ src/test/java/mast/mytest.java
@@ -0,0 +1,10 @@
+package mast;
+
+public class mytest {
+
+    public static void main(String[] args) {
+        System.out.println("123123w12312");
+        System.out.println("123123w12312");
+        System.out.println("123123w12312");
+    }
+}

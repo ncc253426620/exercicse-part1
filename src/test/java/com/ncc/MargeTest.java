b462e5311b003ba03dfadfcfd683e8fb60bc4f2c
牛晨丞 <253426620@qq.com>
2020-07-05 10:06:53 +0800

    练习 我在test中写了，你自己加东西进去

---
+++ src/test/java/com/ncc/MargeTest.java
@@ -0,0 +1,14 @@
+package com.ncc;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author Administrator
+ * @Direction TODO
+ * @data 2020/7/5 9:58
+ */
+public class MargeTest {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
